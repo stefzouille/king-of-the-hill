748ca9a4218092e9ab9df5cde11e3dc6befb6998
stefzouille <contact.lokcy@gmail.com>
2021-05-15 17:58:37 +0200

    add

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # king-of-the-hill
-
-c'est pas la peine de corriger c est juste pour dire je rend klk chose,ca fera gagner du temps.
-c est pas faute d avoir test ms je pige rien du tout...
-cdlt..
+work in progress....
--- king-of-the-hill.sol
+++ king-of-the-hill.sol
@@ -1,61 +1,70 @@
 // SPDX-License-Identifier: MIT
 
+
 pragma solidity ^0.8.0;
 
-// Pour remix il faut importer une url depuis un repository github
-// Depuis un project Hardhat ou Truffle on utiliserait: import "@openzeppelin/ccontracts/utils/Address.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/ERC20.sol";
 import "./Ownable.sol";
 
-
-contract King {
+contract kingOfTheHill {
+    
      // library usage
     using Address for address payable;
     
-    mapping (address => uint256) private _allowances;
-    mapping (address => uint256) _balancesPot;
-    address private _players;
-    address private _owner;
-    uint256 private _initialPot;
-    uint256 private _deposit;
-    uint256 private _percentage;
+    mapping (address => uint256) private _balances;        //solde du S.M.
+    uint256 private _pot;                                  //solde du pot
+    uint256 private _seed;                                 //seed reversé
+    address private _owner;                                //owner du S.M.
+    address private _ownerKing;                            //vainqueur du pot
+    address private _player;                               //joueur qui rentre   
+    uint256 private _numberBloc;
+    uint256 private _offer;                                //mise du joueur
+    uint256 private _counter;                              //compteur du bloc
+    uint256 private _profit;
     
-    
+    // Events
     event Deposited(address indexed sender, uint256 amount);
     
-    constructor(address owner_, uint256 initialPot_) {
-        _owner = owner_;
-        _initialPot = initialPot_;
+    //function modifier
+    modifier ownerKing() {
+        require(msg.value == _pot * 2, "kingOfTheHill: the offer must be greater or equal than twice the pot");
+        _;
     }
     
-        // Function declarations below
+    constructor(address owner_, uint256 numberBloc_) payable {    //initialisation address du owner et nb de bloc au deploy
+        _owner = owner_;
+        _numberBloc = numberBloc_;
+        
+    }
+        
+    
+    // Function declarations below
     receive() external payable {
-        deposit(msg.sender, msg.value);
+        _deposit(msg.sender, msg.value);
     }
 
-    
-    
-    function deposit() external payable {
-       deposit(msg.sender, msg.value);
-       
-    
+    function deposit(address send) external payable {
+      
+        _ownerKing <= send;
+        _deposit(msg.sender, msg.value);
     }
     
-     function deposit(address sender, uint256 amount) private {
-        _balancesPot[sender] += amount;
-        emit Deposited(sender, amount);
-    }
-    
-    function withdraw(uint256 ) public {
-        require(_balancesPot[msg.sender] > 0, "SmartWallet: can not withdraw 0 ether");
-        uint256 amount = _balancesPot[msg.sender];
-        _balancesPot[msg.sender] = 0;
-        _balancesPot[_owner] += amount;
-        payable(msg.sender).transfer(amount - amount);
+    function whoIsKing() public view returns (address) {
+        return _ownerKing;
     }
     
     function total() public view returns (uint256) {
         return address(this).balance;
     }
+
+ 
+    
+    function _deposit(address sender, uint256 amount) private {
+        _balances[sender] += amount;
+        emit Deposited(sender, amount);
+        _ownerKing = sender;
+        
+    }
+    
+    
 }
